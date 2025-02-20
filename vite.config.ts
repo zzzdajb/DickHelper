4d65752d1c7e081eeb759552bbcf612b9ed78911
York <zhuyanfang@outlook.com>
2025-02-20 13:58:35 +0800

    修改构建设置

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 添加这一行
+  base: '/',  // 修改这里
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
