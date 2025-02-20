9f5abd8ffb4203c18e942da32a4387376a855730
York <zhuyanfang@outlook.com>
2025-02-20 14:21:01 +0800

    尝试修复路径问题

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // 修改这里
+  base: '',  // 修改这里
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,  // 添加这行以便调试
-    minify: true
+    sourcemap: false,
+    minify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
