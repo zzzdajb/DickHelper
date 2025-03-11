ce468f00de9425c0b5ce7998f552b2bc1d31dc33
zhzenghui <zhzenghui@gmail.com>
2025-03-11 19:03:27 +0800

    add electron config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
-  }
-})
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
