848eca0cf10e86234d4f99c1ba67ac672c37f89d
zzzdajb <122362831+zzzdajb@users.noreply.github.com>
2025-04-25 15:34:31 +0800

    Apply .gitignore and untrack ignored files

---
+++ DickHelperDesktop/DickHelperDesktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DickHelperDesktop.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
